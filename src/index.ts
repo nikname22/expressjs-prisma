43f2771a1e7093559e681571c605a6cee33778c1
Nikolas Cunha <nikolascunha123@gmail.com>
2023-03-20 00:51:44 -0300

    cors test

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import cors from "cors"
 
 const prisma = new PrismaClient();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/documents", async (req, res) => {
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+
+app.get("/documents", async (req, res) => { 
   const todos = await prisma.documents.findMany({
     orderBy: { createdAt: "desc" },
   });
@@ -18,16 +26,21 @@ app.get("/documents", async (req, res) => {
   res.json(todos);
 });
 
-app.post("/todos", async (req, res) => {
-  const {id, createdAt, description, level, title} = req.body
-  const todo = await prisma.documents.create({
-    data: {
-      id, 
-      createdAt, 
-      description, 
-      level, 
-      title
-    },
+app.post("/documents", async (req, res) => {
+  const {id, createdAt, description, level, title, category} = req.body
+  const todo = await prisma.documents.createMany({
+    data: [
+        { title: 'Tradução de documento médico', description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', category: 'Médica', level: 3 },
+        { title: 'Tradução de documento médico', description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', category: 'Educação', level: 2 },
+        { title: 'Tradução de documento médico', description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', category: 'Padrão', level: 1 },
+        { title: 'Tradução de documento médico', description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', category: 'Médica', level: 3 },
+        { title: 'Tradução de documento médico', description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', category: 'Educação', level: 2 },
+        { title: 'Tradução de documento médico', description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', category: 'Padrão', level: 1 },
+        { title: 'Tradução de documento médico', description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', category: 'Médica', level: 3 },
+        { title: 'Tradução de documento médico', description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', category: 'Educação', level: 2 },
+        { title: 'Tradução de documento médico', description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', category: 'Padrão', level: 1 },
+        { title: 'Tradução de documento médico', description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', category: 'Médica', level: 3 },
+    ],
   });
 
   return res.json(todo);
